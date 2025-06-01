4cedb89a81c90c88a0ea4b7ce4cf0207dd54f091
taylor-ken <kenneth.n.taylor@gmail.com>
2025-05-31 23:58:45 -0400

    start unittesting

--- test_setup.py
+++ test_setup.py
@@ -10,6 +10,19 @@ class TestSetup(unittest.TestCase):
         contestants2 = []
         self.assertEqual(contestants1, contestants2)
 
+    def test_not_eq(self):
+        dir_test_path_1 = "./test_play_folders/test_play_1"
+        dir_test_path_2 = "./test_play_folders/test_play_2"
+        a = get_contestants(dir_test_path_1)
+        b = get_contestants(dir_test_path_2)
+        self.assertNotEqual(a, b)
+
+    def test_not_eq2(self):
+        dir_test_path_1 = "./test_play_folders/test_play_2"
+        dir_test_path_2 = "./test_play_folders/test_play_3"
+        a = get_contestants(dir_test_path_1)
+        b = get_contestants(dir_test_path_2)
+        self.assertNotEqual(a, b)
 
 if __name__ == "__main__":
     unittest.main()
